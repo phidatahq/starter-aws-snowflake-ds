d4f4fbc5a6557acacdce17fddc63ab49f01f6331
Ashpreet Bedi <ashpreet@phidata.com>
2022-11-27 21:59:18 -0500

    update

--- workspace/dev/images/build_jupyter_image.sh
+++ workspace/dev/images/build_jupyter_image.sh
@@ -4,8 +4,8 @@ set -e
 
 CURR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOCKER_FILE="${CURR_SCRIPT_DIR}/jupyter.Dockerfile"
-REPO="repo"
-NAME="jupyter-dp"
+REPO="phidata"
+NAME="jupyter-starter-aws-snowflake-ds"
 TAG="dev"
 
 echo "Running: docker build -t $REPO/$NAME:$TAG -f $DOCKER_FILE ."
